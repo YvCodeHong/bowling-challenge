19a9815c3aacf9ea5f8ca96223635e7c7b3539f0
Yvonne Tang <ytang604@gmail.com>
2018-07-21 13:18:57 +0100

    add empty frame method

--- jasmine-standalone-3.1.0/src/Game.js
+++ jasmine-standalone-3.1.0/src/Game.js
@@ -1,10 +1,14 @@
 'use strict';
 
 function Game () {
-  this.totalscore = 0
-
+  this.totalscore = 0;
+  this.frame = 0
 }
 
 Game.prototype._totalscore = function() {
   return this.totalscore
+}
+
+Game.prototype._frame = function() {
+  return this.frame
 }
